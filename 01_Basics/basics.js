c26c6f744b68bece77a7b63e075dbb9fc4110e54
Dhaval Pandya <96469609+dhavalp5@users.noreply.github.com>
2024-08-28 10:12:25 +0000

    Create file for basics

---
+++ 01_Basics/basics.js
@@ -0,0 +1,18 @@
+let accountName = "Hello"
+const accountId = 10
+
+/* never use var in js */
+
+console.log(accountName);
+console.table([accountId,accountName])
+
+//Datatype conversion
+
+let boolValue = true
+let accountIdInString = String(accountId)
+console.log(accountId)
+console.log(typeof accountIdInString)
+
+let boolValueInNumber = Number(boolValue)
+console.log(boolValue)
+console.log(typeof boolValueInNumber)
