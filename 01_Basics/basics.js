b46df58cc6996fba157ab6566c0c21cf1a78f7f7
Dhaval Pandya <96469609+dhavalp5@users.noreply.github.com>
2024-09-05 06:38:15 +0000

    Loops

--- 01_Basics/basics.js
+++ 01_Basics/basics.js
@@ -92,7 +92,6 @@ function printSecondNumber(getArray){
 // printSecondNumber([10,20,23]);   // directly pass array
 
 
-
 //Passing Object
 function printUserDetails(getObject){
     // console.log(` name: ${getObject.name}, email: ${getObject.email}`)
@@ -116,14 +115,102 @@ const calculate4 = (number1, number2) => ({username: "Dc"}); // Implicit Return 
 
 
 //Example of IIFE (Immidiately Invoked function Expression)
-// (function chai(){
-//     console.log(`hello World`);
-// })();
+(function chai(){
+    // console.log(`hello World`);
+})();
 
-// ( ()  => {
-//     console.log(`hello World`);
-// })();
+( ()  => {
+    // console.log(`hello World`);
+})();
 
 ( (name)  => {
-    console.log(`${name}`);
+//     console.log(`${name}`);
 })("Dc");
+
+
+//++++++++++++++++ For Of loop ++++++++++++++++
+//iteratable for all elements like array and map except object
+for (const element of myArr) {
+    // console.log(element);
+}
+
+let userName = newObj.name;
+for (const name of userName) {
+    // console.log(`single character is ${name}`);
+}
+
+// MAP
+//To add unique key value pair map is used, 
+//duplication is not allowed
+
+const map = new Map()
+map.set("IN", "India")
+map.set("FR", "France")
+map.set("IT", "Italy")
+
+//to print complete map
+for (const key of map) {
+    // console.log(key)
+}
+
+//To print individual key and value with array destructuring
+for (const [key,value] of map) {
+    // console.log(`Key = ${key} and Value = ${value} `)
+}
+
+//++++++++++++++++ For In loop ++++++++++++++++
+//iteratable for all elements like array and object except map
+//FOROF loop gives directly value where as FORIN loop will give key directly
+
+for (const key in newObj) {
+    // console.log(`key is ${key} and value is ${newObj[key]}`);
+}
+
+for (const key in myArr) {
+    // console.log(`key is ${key} and value is ${myArr[key]}`);
+}
+
+//++++++++++++++++ For Each loop ++++++++++++++++
+
+string_array = ["js", "react", ".net", "aws", "azure"]
+
+string_array.forEach((key) => {
+    // console.log(key);
+});
+
+// Print whole array by foreach loop
+string_array.forEach((key,index,arr) => {
+    //  console.log(`Key is ${key} on Index ${index}, whole array is ${arr} `);
+});
+
+
+
+//++++++++++++++++ Filters and map ++++++++++++++++
+
+// array of object
+const books = [{title: "book1", genre: "History", publish: 1992},
+    {title: "book2", genre: "Science", publish: 1995},
+    {title: "book3", genre: "Fiction", publish: 1998},
+    {title: "book4", genre: "Non-Fiction", publish: 2001}
+]
+
+//return automatically if we are not adding {}
+let filteredBooks = books.filter( (bk) => bk.publish >= 1998)
+// console.log(filteredBooks);
+
+//if we add {} then have to add return keyword
+filteredBooks = books.filter( (bk) => {return bk.publish > 1998})
+// console.log(filteredBooks);
+
+
+//MAP()
+mappedNumbers = myArr.map( (num) => num + 10);
+// console.log(mappedNumbers);
+
+// chaining
+let newNums = myArr
+.map((num) => num * 100)
+.map((num) => num + 1)
+.filter((num) => num <= 100);
+
+// console.log(newNums);
